8b60c6b449f17c84b3577a501f6e3c22a542a88c
Bineth Atukorala <bineth.mandiv@gmail.com>
2021-05-03 15:49:30 +0530

    Implemented function to check whether a point is in the triangle and made areaHeron() camel case (to area_heron())

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fmt;
 
 // Structure of a Point
 
+#[derive(Copy, Clone)]
 struct Point {
     x: i32,
     y: i32,
@@ -46,15 +47,26 @@ impl Triangle {
     }
 
     // Find area of a triangle 
-    fn area(&self, perimeter: f32) -> f32 {
+    fn area(&self) -> f32 {
         ((self.a.x*(self.b.y-self.c.y) + self.b.x*(self.c.y - self.a.y) + self.c.x*(self.a.y - self.b.y)) as f32 /2.0).abs()
     }
 
     // Find area of a triangle with Heron's Formula
-    fn areaHeron(&self, perimeter:f32) -> f32 {
+    fn area_heron(&self, perimeter:f32) -> f32 {
         let p = perimeter/(2 as f32);
         f32::sqrt(p * (p-self.a.distance(&self.b)) * (p-self.b.distance(&self.c)) * (p-self.c.distance(&self.a)))
     }
+
+    // Check whether a point is inside a triangle.
+    // This is done by creating smaller triangles with the point and seeing whether `ABC = ABP + PBC + APC`
+    fn is_point_inside(&self, point: &Point) -> bool {
+        let tr_a = Triangle{a: self.a, b: self.b, c: *point};
+        let tr_b = Triangle{a: *point, b: self.b, c: self.c};
+        let tr_c = Triangle{a: self.a, b: *point, c: self.c};
+
+        self.area() == tr_a.area() + tr_b.area() + tr_c.area()
+    }
+
 }
 
 fn main() {
@@ -70,5 +82,11 @@ fn main() {
 
     println!("Points of the triangle : {}", tr);
     println!("The length of the three sides of the triangle : {} {} {}", tr.a.distance(&tr.b), tr.b.distance(&tr.c), tr.c.distance(&tr.a));
-    println!("Area of the triangle is : {}", tr.area(tr.perimeter()));
+    println!("Perimeter of the triangle is : {}", tr.area());
+    println!("Area of the triangle is : {}", tr.area());
+    println!("Area of the triangle is using Heron's Formula : {}", tr.area_heron(tr.perimeter()));
+    
+    let random_point = Point{x: 4, y: 3};
+    
+    println!("Is the point {} in the triangle? : {}", random_point, tr.is_point_inside(&random_point));
 }
